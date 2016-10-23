f7618a1349b1026b74b4272021136955a4b0e945
andijluke <andi.luke@gmail.com>
2016-10-23 15:45:47 -0700

    made it through chapter 3 of tutorial master/detail

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,14 +5,23 @@ export class Babyname {
     name: string;
 }
 
+const BABYNAMES: Babyname[] = [
+  { id: 10, name: 'Violet'},
+  { id: 11, name: 'Sequoia' },
+  { id: 12, name: 'Sienna' },
+  { id: 13, name: 'Sierra' },
+  { id: 14, name: 'Tara' }
+];
+
 @Component({
   selector: 'my-app',
   templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
     title = 'Baby Name Tracker';
-    babyname: Babyname = {
-        id: 1,
-        name: 'Violet'
-    };
+    selectedBabyname: Babyname;
+    babynames = BABYNAMES;
+    onSelect(babyname: Babyname): void {
+        this.selectedBabyname = babyname;
+    }
  }
